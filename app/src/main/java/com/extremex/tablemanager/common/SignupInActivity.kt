ca1786777edbc2fc807e85907ad5a951ab147c01
maynarddacosta <dacostasmaynard5@gmail.com>
2023-10-25 14:29:15 +0100

    login attached

--- app/src/main/java/com/extremex/tablemanager/common/SignupInActivity.kt
+++ app/src/main/java/com/extremex/tablemanager/common/SignupInActivity.kt
@@ -2,17 +2,14 @@ package com.extremex.tablemanager.common
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Message
 import androidx.fragment.app.Fragment
 import com.extremex.tablemanager.R
 import com.extremex.tablemanager.common.fragment.LoginFragment
 import com.extremex.tablemanager.common.fragment.SignUpFragment
 import com.extremex.tablemanager.lib.PopUpBox
 import com.google.firebase.Firebase
-import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.database
 
 class SignupInActivity : AppCompatActivity(),LoginFragment.AccountClickListener, SignUpFragment.SignupListener {
@@ -42,15 +39,16 @@ class SignupInActivity : AppCompatActivity(),LoginFragment.AccountClickListener,
         user :FirebaseUser, firstName: String, lastName: String, DOB: String, Id : Int, phNum: String, email: String, isTeacher: Boolean
     ) {
         val UID = user.uid
-        val database = Firebase.database("https://table-manager-25147-default-rtdb.asia-southeast1.firebasedatabase.app/")
-        val databaseReference = Firebase.database.getReferenceFromUrl("https://table-manager-25147-default-rtdb.asia-southeast1.firebasedatabase.app/")
-        databaseReference.child("users").child(UID)
-        databaseReference.child("users").child(UID).child("FirstName").setValue(firstName)
-        databaseReference.child("users").child(UID).child("LastName").setValue(lastName)
-        databaseReference.child("users").child(UID).child("DateOfBirth").setValue(DOB)
-        databaseReference.child("users").child(UID).child("UniqueID").setValue(Id)
-        databaseReference.child("users").child(UID).child("PhoneNumber").setValue(phNum)
-        databaseReference.child("users").child(UID).child("Email").setValue(email)
+        database = Firebase.database.getReferenceFromUrl("https://table-manager-25147-default-rtdb.firebaseio.com")
+        database.child("users").child(UID)
+        database.child("users").child(UID).child("FirstName").setValue(firstName)
+        database.child("users").child(UID).child("LastName").setValue(lastName)
+        database.child("users").child(UID).child("DateOfBirth").setValue(DOB)
+        database.child("users").child(UID).child("UniqueID").setValue(Id)
+        database.child("users").child(UID).child("PhoneNumber").setValue(phNum)
+        database.child("users").child(UID).child("Email").setValue(email)
+
+
 
 
 
